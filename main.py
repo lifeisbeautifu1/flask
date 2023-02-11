2c56adc243cd1c6dac21e6ba47d06d5c77873e90
Potato <poltoradnevone@gmail.com>
2023-02-11 06:46:17 +0400

    test

--- main.py
+++ main.py
@@ -1,13 +1,99 @@
-from flask import Flask, jsonify
+import sys
+from PIL import Image as im
+import cv2
 import os
+import tensorflow as tf
+import numpy as np
+from flask import Flask, request, send_file
+from flask_cors import CORS
+from flask_restful import Api
+
+
+ridnet=tf.keras.models.load_model('noise.h5')
+
+def get_patches(file_name,patch_size,crop_sizes):
+    '''This functions creates and return patches of given image with a specified patch_size'''
+    image = cv2.imread(file_name) 
+    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    height, width , channels= image.shape
+    patches = []
+    for crop_size in crop_sizes: #We will crop the image to different sizes
+        crop_h, crop_w = int(height*crop_size),int(width*crop_size)
+        image_scaled = cv2.resize(image, (crop_w,crop_h), interpolation=cv2.INTER_CUBIC)
+        for i in range(0, crop_h-patch_size+1, patch_size):
+            for j in range(0, crop_w-patch_size+1, patch_size):
+              x = image_scaled[i:i+patch_size, j:j+patch_size] # This gets the patch from the original image with size patch_size x patch_size
+              patches.append(x)
+    return patches
+
+def predict_fun(model,image_path,noise_level=30):
+  #Creating patches for test image
+  patches=get_patches(image_path,40,[1])
+  test_image=cv2.imread(image_path)
+
+  patches=np.array(patches)
+  ground_truth=create_image_from_patches(patches,test_image.shape)
+
+  #predicting the output on the patches of test image
+  patches = patches.astype('float32') /255.
+  # patches_noisy = patches+ tf.random.normal(shape=patches.shape,mean=0,stddev=noise_level/255) 
+  # patches_noisy = tf.clip_by_value(patches_noisy, clip_value_min=0., clip_value_max=1.)
+  # noisy_image=create_image_from_patches(patches_noisy,test_image.shape)
+
+  denoised_patches=model.predict(patches)
+  denoised_patches=tf.clip_by_value(denoised_patches, clip_value_min=0., clip_value_max=1.)
+
+  #Creating entire denoised image from denoised patches
+  denoised_image=create_image_from_patches(denoised_patches,test_image.shape)
+
+  return denoised_patches,ground_truth/255.,denoised_image
+
+def create_image_from_patches(patches,image_shape):
+  '''This function takes the patches of images and reconstructs the image'''
+  image=np.zeros(image_shape) # Create a image with all zeros with desired image shape
+  patch_size=patches.shape[1]
+  p=0
+  for i in range(0,image.shape[0]-patch_size+1,patch_size):
+    for j in range(0,image.shape[1]-patch_size+1,patch_size):
+      image[i:i+patch_size,j:j+patch_size]=patches[p] # Assigning values of pixels from patches to image
+      p+=1
+  return np.array(image)
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpeg', 'jpg'}
 
 app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+api = Api(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'bad request'
 
+        file = request.files['file']
+
+        if file.filename == '':
+            return 'no selected file'
+
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            denoised_patches,ground_truth,denoised_image=predict_fun(ridnet,os.path.join(app.config['UPLOAD_FOLDER'], filename),noise_level=30)
+            denoised_image = denoised_image * 255
+            denoised_image = denoised_image.astype(np.uint8)
+            data = im.fromarray(denoised_image)
+            data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            return 'not allowed format'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
